8bdd01751598b79cbc84639d00a2dc79609f7d3e
krislee94 <575558044@qq.com>
2020-03-26 09:37:23 +0000

    Deploying to gh-pages from  - c2cdbdf40032177cf4bcdd83c226eda02d01452d 🚀

---
+++ precache-manifest.9267d2c2771638435786a4758e7c2935.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af805b22039da90530290f6fd79f46c0",
+    "url": "/krisblog/index.html"
+  },
+  {
+    "revision": "d1c4b40f356a717576c2",
+    "url": "/krisblog/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca6d149d0438a0a535ef",
+    "url": "/krisblog/static/js/2.af1e001d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/krisblog/static/js/2.af1e001d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1c4b40f356a717576c2",
+    "url": "/krisblog/static/js/main.9e9ed4d9.chunk.js"
+  },
+  {
+    "revision": "027aebaebb32415cacbe",
+    "url": "/krisblog/static/js/runtime-main.043e7c67.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/krisblog/static/media/logo.5d5d9eef.svg"
+  }
+]);
