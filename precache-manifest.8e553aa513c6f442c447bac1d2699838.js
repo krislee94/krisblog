ed7c9e07758589ba63da10dd7023120fef49920c
krislee94 <575558044@qq.com>
2020-08-17 02:50:35 +0000

    Deploying to gh-pages from  @ 571a4ba4458e05ff1397bba70651e8650dadeb21 🚀

---
+++ precache-manifest.8e553aa513c6f442c447bac1d2699838.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa726f1daaef27c94482759ac0fec0b",
+    "url": "/krisblog/index.html"
+  },
+  {
+    "revision": "8145bcabe93b23d9f08f",
+    "url": "/krisblog/static/css/2.2f75dd9a.chunk.css"
+  },
+  {
+    "revision": "24ceeb5fe7563d37ab4c",
+    "url": "/krisblog/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8145bcabe93b23d9f08f",
+    "url": "/krisblog/static/js/2.7c8d9f4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/krisblog/static/js/2.7c8d9f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ceeb5fe7563d37ab4c",
+    "url": "/krisblog/static/js/main.e11b73af.chunk.js"
+  },
+  {
+    "revision": "ffb104c681f6a7fc88a8",
+    "url": "/krisblog/static/js/runtime-main.0ab0faf9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/krisblog/precache-manifest.40576841ad87823165cff5d416d54fc4.js"
+  "/krisblog/precache-manifest.8e553aa513c6f442c447bac1d2699838.js"
 );
 
 self.addEventListener('message', (event) => {
